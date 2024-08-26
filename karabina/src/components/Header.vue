ceda62aca4867a6a93b6136126f9083d15864e88
Okkyyt <okky.161227@gmail.com>
2024-08-26 15:21:14 +0900

    add src

---
+++ karabina/src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <header class="header">
+    <h1>奈良MAP</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  data() {
+    return {
+      message: "Hello, World!",
+    };
+  },
+};
+</script>
+
+<style scoped>
+.header {
+  background-color: burlywood;
+  color: black;
+  text-align: center;
+  padding: 5px;
+}
+</style>
