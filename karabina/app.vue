cdd3d02f5cb3d839d3fbfba2899c0fd851cb4aeb
Okkyyt <okky.161227@gmail.com>
2024-08-26 14:20:36 +0900

    add NuxtPage

--- karabina/app.vue
+++ karabina/app.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
-    <div>hello world</div>
+    <Header></Header>
+    <NuxtPage></NuxtPage>
   </div>
 </template>
+
+<script>
+import Header from '~/components/Header.vue';
+
+export default {
+  components: {
+    Header
+  }
+}
+</script>
---
+++ karabina/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+    <header class="header">
+        <h1>/header</h1>   
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    data() {
+        return {
+            message: 'Hello, World!'
+        }
+    }
+}
+</script>
