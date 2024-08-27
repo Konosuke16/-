e80896e720d1701dc85398bb4a7e637136362f98
usagi8mayu <usagi8mayu@docomo.ne.jp>
2024-08-28 00:07:07 +0900

    最初のページ

--- karabina/src/pages/index.vue
+++ karabina/src/pages/index.vue
@@ -1,5 +1,30 @@
 <template>
   <div>
-    <h1>/index</h1>
-  </div>
+   <div class="image-container">
+   <img src="C:\Users\usagi\Downloads\Adore The World 世界を旅する.png" alt="奈良の風景" class="image">
+   </div>
+   <h1 class="headline">奈良で旬の体験を</h1>
+ </div>
 </template>
+
+
+
+
+  <style scoped>
+  .headline {
+    font-size: 2rem; /* 文字の大きさ */
+    color: #333; /* 文字の色 */
+    text-align: center; /* テキストを中央揃え */
+    padding: 1rem; /* 周りのスペース */
+    font-family: 'Arial', sans-serif; /* フォントの設定 */
+    text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.1); /* 文字の影を追加 */
+  }
+  </style>
+
+
+
+
+
+
+
+  
