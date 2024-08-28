8d97fb078f6a8860fafbb4bf310b2c9577b1018a
Okkyyt <okky.161227@gmail.com>
2024-08-28 21:53:45 +0900

    fetchできなぁい

--- karabina/nuxt.config.ts
+++ karabina/nuxt.config.ts
@@ -10,4 +10,9 @@ export default defineNuxtConfig({
     },
   },
   srcDir: "src/",
+  nitro: {
+    externals: {
+      inline: ["pg"],
+    },
+  },
 });
--- karabina/src/pages/test.vue
+++ karabina/src/pages/test.vue
@@ -3,27 +3,29 @@
     <h1>イベント一覧</h1>
     <ul>
       <li v-for="event in events" :key="event.id">
-        {{ event.name }} - {{ event.date }} - {{ event.location }}
+        {{ event.name }} - {{ formatDate(event.date) }} - {{ event.location }}
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-  data() {
-    return {
-      events: [], // ここで `events` プロパティを定義
-    };
-  },
-  async asyncData({ $axios }) {
+  async asyncData() {
     try {
-      const events = await $axios.$get("/api/getEvents");
-      return { events };
-    } catch (err) {
-      console.error(err);
+      const res = await axios.get("/api/getEvents");
+      return { events: res.data };
+    } catch (error) {
+      console.error("Failed to fetch events:", error);
       return { events: [] };
     }
+  },
+  methods: {
+    formatDate(date) {
+      return new Date(date).toLocaleDateString();
+    },
   },
 };
 </script>
